336d8260a5de68259d7c2d57c7b901ae8f793c83
Edward Coddington <ed.coddington@gmail.com>
2021-09-02 16:05:56 -0400

    update vi client provided list

--- public/js/site.js
+++ public/js/site.js
@@ -2,3 +2,7 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 });
+
+const navbar = document.querySelector('#subnav');
+const scrollspy = VanillaScrollspy(navbar,300);
+scrollspy.init();
